86e6a33ca38f0776ae384f32ba203e9fdd4aceed
Dave Woodall <dave.woodall@livingsocial.com>
2014-09-06 16:59:39 -0600

    toying around

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = angular.module('bio', ['ngRoute']);
+
+app.controller('homeController', function($scope, five, about){
+
+});
+
+app.controller('wifeController', function($scope){
+  $scope.photo = './danae.jpg';
+});
+
+app.controller('bioController', function($scope, five, about){
+  $scope.bio = {
+    name: 'Dave',
+    age: 37
+  }
+  $scope.greeting = 'hello world!';
+  $scope.fivrr = five;
+  $scope.background = about.blurb;
+})
+
+app.value('five', 5);
+
+app.service('about', function(){
+  return {
+    blurb: 'this is bio and I know that a service is probably a bit much.'
+  }
+});
+
+app.config([ '$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/', {
+      controller: 'homeController',
+      template: 'BAM!'
+    }).
+    when('/wife', {
+      templateUrl: 'wife.html',
+      controller: 'wifeController'
+    }).
+    when('/bio', {
+      templateUrl: 'bio.html',
+      controller: 'bioController'
+    })
+}]);
---
+++ todo.md
@@ -0,0 +1,5 @@
+- navigate to a picture.
+- add text to a form
+- use a service
+- write a custom directive
+- get a movie from youtube api
